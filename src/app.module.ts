c8f053363924bc9a2a6aa08de4e817fabc96f32c
ngimdock <ngimdockzemfack@gmail.com>
2022-12-15 16:28:27 +0100

    Déplacement du module GoogleDrive dans appModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MulterModule } from '@nestjs/platform-express';
+import { GoogleDriveModule } from 'nestjs-google-drive';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [MulterModule.register({}), UsersModule],
+  imports: [
+    MulterModule.register({}),
+    ConfigModule.forRoot({}),
+    GoogleDriveModule.register({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      redirectUrl: process.env.GOOGLE_REDIRECT_URL,
+      refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
